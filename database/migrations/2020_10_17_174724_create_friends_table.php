50200402fd351cbb87575f2b6ec40516abfe3741
nancyhossam <48593806+nancyhossam@users.noreply.github.com>
2021-01-19 01:13:02 +0200

    Add files via upload

---
+++ database/migrations/2020_10_17_174724_create_friends_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->integer('friend1');
+            $table->integer('friend2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
